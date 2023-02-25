c1f2657b654257de6b204c8e555c87b898f3b28e
Petar Radosevic <petar@petar.dev>
2023-02-25 07:18:14 +0100

    lift-off 🚀

---
+++ lua/petar/init.lua
@@ -0,0 +1,7 @@
+require("petar.remap")
+require("petar.set")
+
+-- have a cleaner netrw
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
---
+++ lua/petar/remap.lua
@@ -0,0 +1,31 @@
+vim.g.mapleader = " " -- space as leader
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- manage the filesystem
+
+-- easily move blocks of code around in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- cursor stays in place when joining lines
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- jumping around keeps the cursor in the middle
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- same for search terms, keep the cursor in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- keep the original when pasting
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- yank into system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- quickfix list navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
